9c5da2d91b5d581d1800409ec6fb9eb6c45e9e04
Le Khau Huu Tai <lekhauhuutai48@gmail.com>
2024-03-26 19:44:08 +0700

    Adding UDP project. #1

---
+++ UDPClient/UDPClient/Program.cs
@@ -0,0 +1,31 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+        IPAddress broadcast = IPAddress.Parse("192.168.0.102");
+        Console.WriteLine("Enter message to sent the broadcast address");
+        var message = Console.ReadLine();
+
+        while (message != string.Empty)
+        {
+            byte[] sendbuf = Encoding.ASCII.GetBytes(message);
+            IPEndPoint ep = new IPEndPoint(broadcast, 11000);
+            s.SendTo(sendbuf, ep);
+
+            Console.WriteLine("Message sent to the broadcast address");
+
+            message = Console.ReadLine();
+        }
+
+        Console.ReadLine();
+    }
+}
---
+++ UDPClient/UDPServer/Program.cs
@@ -0,0 +1,42 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class UDPListener
+{
+    private const int listenPort = 11000;
+
+    private static void StartListener()
+    {
+        UdpClient listener = new UdpClient(listenPort);
+        IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, listenPort);
+
+        try
+        {
+            while (true)
+            {
+                Console.WriteLine("Waiting for broadcast");
+                byte[] bytes = listener.Receive(ref groupEP);
+
+                Console.WriteLine($"Received broadcast from {groupEP} :");
+                Console.WriteLine($" {Encoding.ASCII.GetString(bytes, 0, bytes.Length)}");
+            }
+        }
+        catch (SocketException e)
+        {
+            Console.WriteLine(e);
+        }
+        finally
+        {
+            listener.Close();
+        }
+    }
+
+    public static void Main()
+    {
+        StartListener();
+    }
+}
